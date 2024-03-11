529ba030974c450116d89ce9eba109d2e53ea8ac
HikariVoss <hikarivoss.acc@gmail.com>
2024-03-11 00:03:24 +0000

    split main.js into multiple files

--- 02 Webpack/src/hex-verter/main.js
+++ 02 Webpack/src/hex-verter/main.js
@@ -1,58 +1,6 @@
-const hex_lookup = [
-    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"
-];
-
-function dec_to_binary(number) {
-    var digits = [];
-
-    while (number > 0) {
-        var digit = String(number % 2);
-        number = (number - (number % 2))/ 2;
-        digits.push(digit);
-    }
-
-    return digits.reverse().join("");
-}
-
-function group_binary(number) {
-    var digits = [];
-
-    const number_split = number.split("").reverse();
-    var group_count = (number_split.length - (number_split.length % 4)) / 4 + 1;
-    if (number_split.length % 4 == 0) {
-        group_count -= 1;
-    }
-
-    var group = []
-
-    for (let i = 0; i < number_split.length; i++) {
-        group.push(number_split[i]);
-        if (group.length == 4) {
-            digits.push(group.reverse().join(""));
-            group = [];
-        }
-    }
-
-    for (let i = number_split.length; i < 4 * group_count; i++) {
-        group.push("0");
-        if (group.length == 4) {
-            digits.push(group.reverse().join(""));
-            group = [];
-        }
-    }
-
-    return digits.reverse();
-}
-
-function binary_to_hex(number) {
-    var result = 0;
-
-    for (let i = 0; i < 4; i++) {
-        result += Number(number[i]) * 2**(3 - i);
-    }
-
-    return result;
-}
+import {hex_lookup} from "./library/constants/lookup-tables";
+import {dec_to_binary, binary_to_hex} from "./library/conversions";
+import { group_binary } from "./library/formatting";
 
 const decimal_input = document.getElementById("decimalInput");
 const conversion_button = document.getElementById("convert-button");
@@ -63,11 +11,11 @@ const hex_output = document.getElementById("hexadecimal");
 
 const click = () => {
 
-    binary = dec_to_binary(Number(decimal_input.value));
+    const binary = dec_to_binary(Number(decimal_input.value));
     binary_step.innerText = "binary: " + binary;
     
-    tempStr = "grouped: ";
-    grouped_binary = group_binary(binary);
+    var tempStr = "grouped: ";
+    const grouped_binary = group_binary(binary);
     for (let i = 0; i < grouped_binary.length; i++) {
         tempStr += grouped_binary[i] + " ";
     }
